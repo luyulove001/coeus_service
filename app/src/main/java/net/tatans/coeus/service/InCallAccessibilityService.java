d48d7a636889e405c6d63a4685b21aacb7b5999c
chenluyu <540302096@qq.com>
2016-04-13 11:23:55 +0800

    将默认短信辅助功能集合到天坦服务

--- app/src/main/java/net/tatans/coeus/service/InCallAccessibilityService.java
+++ app/src/main/java/net/tatans/coeus/service/InCallAccessibilityService.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 
+import java.util.List;
+
 
 /**
  * Created by John on 2016/1/5.
@@ -27,10 +29,23 @@ public class InCallAccessibilityService extends AccessibilityService {
                                         if (rowNode == null) {
                                                             return;
                                         } else {
+                                            if ("com.android.dialer".equals(event.getPackageName()))
                                                             recycle(rowNode);
                                         }
+                        this.processAccessibilityEnvent(event);
 
                     }
+
+    private void processAccessibilityEnvent(AccessibilityEvent event) {
+
+        Log.d("test", event.eventTypeToString(event.getEventType()));
+        if (event.getSource() == null) {
+            Log.d("test", "the source = null");
+        } else {
+//            Log.d("test", "event = " + event.toString());
+            processKillApplication(event);
+        }
+    }
 
                     @Override
                     public void onInterrupt() {
@@ -65,6 +80,38 @@ public class InCallAccessibilityService extends AccessibilityService {
                     }
 
 
+    private void processKillApplication(AccessibilityEvent event) {
 
+        if (event.getSource() != null) {
+            if (event.getPackageName().equals("com.android.settings")) {
+                List<AccessibilityNodeInfo> stop_nodes = event.getSource().findAccessibilityNodeInfosByText("默认短信");
+                if (stop_nodes != null && !stop_nodes.isEmpty()) {
+                    List<AccessibilityNodeInfo> ok_nodes = event.getSource().findAccessibilityNodeInfosByText("确定");
+                    if (ok_nodes != null && !ok_nodes.isEmpty()) {
+                        AccessibilityNodeInfo node;
+                        for (int i = 0; i < ok_nodes.size(); i++) {
+                            node = ok_nodes.get(i);
+                            if (node.getClassName().equals("android.widget.Button")) {
+                                node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                                Log.d("action", "click ok");
+                            }
+                        }
+                    }
+                    List<AccessibilityNodeInfo> xiayibu_nodes = event.getSource().findAccessibilityNodeInfosByText("下一步");
+                    if (xiayibu_nodes != null && !xiayibu_nodes.isEmpty()) {
+                        AccessibilityNodeInfo node;
+                        for (int i = 0; i < xiayibu_nodes.size(); i++) {
+                            node = xiayibu_nodes.get(i);
+                            if (node.getClassName().equals("android.widget.Button")) {
+                                node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                                FxService.interrupt(0);
+                                Log.d("action", "click ok");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
