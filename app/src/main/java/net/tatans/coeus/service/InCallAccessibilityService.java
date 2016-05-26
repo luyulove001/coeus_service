2cd7bc69a31525d90a170b707180626d40117a6f
xinbin <287078489@qq.com>
2016-05-26 09:04:05 +0800

    系统设置中极简模式增加警告窗口

--- app/src/main/java/net/tatans/coeus/service/InCallAccessibilityService.java
+++ app/src/main/java/net/tatans/coeus/service/InCallAccessibilityService.java
@@ -3,8 +3,13 @@ package net.tatans.coeus.service;
 import android.accessibilityservice.AccessibilityService;
 import android.annotation.SuppressLint;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+
+import net.tatans.coeus.util.FloatView;
 
 import java.util.List;
 
@@ -13,28 +18,42 @@ import java.util.List;
  * Created by John on 2016/1/5.
  */
 public class InCallAccessibilityService extends AccessibilityService {
-                    public static Boolean flag = false;
-                    public static Boolean closed = true;
+    public static Boolean flag = false;
+    public static Boolean closed = true;
 
-                    @Override
-                    protected void onServiceConnected() {
-                                        super.onServiceConnected();
-                                        Log.d("myTag", "connected");
-                    }
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.d("myTag", "connected");
+    }
 
-                    @SuppressLint("NewApi")
-                    @Override
-                    public void onAccessibilityEvent(AccessibilityEvent event) {
-                                        AccessibilityNodeInfo rowNode = getRootInActiveWindow();
-                                        if (rowNode == null) {
-                                                            return;
-                                        } else {
-                                            if ("com.android.dialer".equals(event.getPackageName()))
-                                                            recycle(rowNode);
-                                        }
-                        this.processAccessibilityEnvent(event);
+    @SuppressLint("NewApi")
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        AccessibilityNodeInfo rowNode = getRootInActiveWindow();
+        if (rowNode == null) {
+            return;
+        } else {
+            if ("com.android.dialer".equals(event.getPackageName()))
+                recycle(rowNode);
+        }
 
-                    }
+
+        switch (event.getEventType()) {
+            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED:
+            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
+            case AccessibilityEvent.TYPE_VIEW_CLICKED:
+            case AccessibilityEvent.TYPE_VIEW_LONG_CLICKED:
+                if (event.getPackageName().equals("com.android.settings")) {
+                    Log.e("acess", "onAccessibilityEvent ");
+                    easySettingApplication(getApplication(), getRootInActiveWindow());
+                }
+        }
+
+
+        this.processAccessibilityEnvent(event);
+
+    }
 
     private void processAccessibilityEnvent(AccessibilityEvent event) {
 
@@ -47,37 +66,37 @@ public class InCallAccessibilityService extends AccessibilityService {
         }
     }
 
-                    @Override
-                    public void onInterrupt() {
+    @Override
+    public void onInterrupt() {
 
-                    }
+    }
 
-                    @SuppressLint("InlinedApi")
-                    public void recycle(AccessibilityNodeInfo info) {
-                                        //通话界面第一次进来，作点击事件，打开拨号键盘
-                                        CharSequence ContentDescription = info.getContentDescription();
-                                        if(ContentDescription != null &&ContentDescription.equals("*")){
-                                                            closed=false;
-                                        }
-                                        if (ContentDescription != null && ContentDescription.equals("拨号键盘")) {
-                                                            if (info.isClickable() && flag &&closed) {
+    @SuppressLint("InlinedApi")
+    public void recycle(AccessibilityNodeInfo info) {
+        //通话界面第一次进来，作点击事件，打开拨号键盘
+        CharSequence ContentDescription = info.getContentDescription();
+        if (ContentDescription != null && ContentDescription.equals("*")) {
+            closed = false;
+        }
+        if (ContentDescription != null && ContentDescription.equals("拨号键盘")) {
+            if (info.isClickable() && flag && closed) {
 
-                                                                                info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
-                                                                                flag = false;
-                                                                                return;
-                                                            }
-                                        }
+                info.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                flag = false;
+                return;
+            }
+        }
 
-                                        if (info.getChildCount() == 0) {
+        if (info.getChildCount() == 0) {
 
-                                        } else {
-                                                            for (int i = info.getChildCount() - 1; i >= 0; i--) {
-                                                                                if (info.getChild(i) != null) {
-                                                                                                    recycle(info.getChild(i));
-                                                                                }
-                                                            }
-                                        }
-                    }
+        } else {
+            for (int i = info.getChildCount() - 1; i >= 0; i--) {
+                if (info.getChild(i) != null) {
+                    recycle(info.getChild(i));
+                }
+            }
+        }
+    }
 
 
     private void processKillApplication(AccessibilityEvent event) {
@@ -111,6 +130,47 @@ public class InCallAccessibilityService extends AccessibilityService {
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * xzb   极简模式覆盖窗口
+     * @param application
+     * @param accessibilityNodeInfo
+     */
+    public void easySettingApplication(android.app.Application application, final AccessibilityNodeInfo accessibilityNodeInfo) {
+        if (accessibilityNodeInfo != null) {
+            final List<AccessibilityNodeInfo> ok_nodes = accessibilityNodeInfo.findAccessibilityNodeInfosByText("极简模式设置");
+            if (!ok_nodes.isEmpty()) {
+                AccessibilityNodeInfo node;
+                for (int i = 0; i < ok_nodes.size(); i++) {
+                    node = ok_nodes.get(i);
+                    if (node.getClassName().equals("android.widget.TextView") && node.isEnabled()) {
+                        WindowManager.LayoutParams lp = FloatView.createDefaultLayoutParams();
+                        lp.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                        lp.height = ViewGroup.LayoutParams.MATCH_PARENT;
+                        final View settingView = FloatView.createFloatView(application, R.layout.easysetting, lp);
+                        settingView.findViewById(R.id.out).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                FloatView.destoryView();
+                                accessibilityNodeInfo.getContentDescription();
+                                List<AccessibilityNodeInfo> rl_item = accessibilityNodeInfo.findAccessibilityNodeInfosByViewId("miui:id/up");
+                                Log.e("**", "onClick rl_item" + rl_item.size());
+                                rl_item.get(0).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                            }
+                        });
+                        settingView.findViewById(R.id.in).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                FloatView.destoryView();
+                            }
+                        });
+
+                    }
+                }
+            }
+
         }
     }
 
---
+++ app/src/main/java/net/tatans/coeus/util/FloatView.java
@@ -0,0 +1,84 @@
+package net.tatans.coeus.util;
+
+import android.app.Application;
+import android.graphics.PixelFormat;
+import android.support.annotation.LayoutRes;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+
+
+/**
+ * Created by newre on 2016/4/25.
+ */
+public class FloatView {
+
+    //定义浮动窗口布局
+    static View mFloatLayout;
+    static WindowManager mWindowManager;
+
+    public static View createFloatView(Application application, @LayoutRes int resource) {
+        return createFloatView(application,resource,createDefaultLayoutParams());
+    }
+
+    public static View createFloatView(Application application, @LayoutRes int resource,WindowManager.LayoutParams wmParams) {
+        if(mWindowManager==null)
+            //获取WindowManagerImpl.CompatModeWrapper
+            mWindowManager = (WindowManager) application.getSystemService(application.WINDOW_SERVICE);
+
+        destoryView();
+
+        /*// 设置悬浮窗口长宽数据
+        wmParams.width = 200;
+        wmParams.height = 80;*/
+
+        //获取浮动窗口视图所在布局
+        LayoutInflater inflater = LayoutInflater.from(application);
+        mFloatLayout = inflater.inflate(resource, null);
+        mWindowManager.addView(mFloatLayout,wmParams);
+
+        mFloatLayout.measure(View.MeasureSpec.makeMeasureSpec(0,
+                View.MeasureSpec.UNSPECIFIED), View.MeasureSpec
+                .makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        return mFloatLayout;
+    }
+
+    public static  WindowManager.LayoutParams createDefaultLayoutParams(){
+        //创建浮动窗口设置布局参数的对象
+        WindowManager.LayoutParams wmParams = new WindowManager.LayoutParams();
+
+        //设置window type
+        wmParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
+        //设置图片格式，效果为背景透明
+        wmParams.format = PixelFormat.RGBA_8888;
+        //设置浮动窗口不可聚焦（实现操作除浮动窗口外的其他可见窗口的操作）
+        wmParams.flags =
+////          LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+////          LayoutParams.FLAG_NOT_TOUCHABLE
+//        ;
+                WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM;
+
+        //调整悬浮窗显示的停靠位置为左侧置顶
+        wmParams.gravity = /*Gravity.LEFT |*/ Gravity.TOP;
+
+        // 以屏幕左上角为原点，设置x、y初始值
+        wmParams.x = 0;
+        wmParams.y = 0;
+        //设置悬浮窗口长宽数据
+        wmParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+        wmParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        return wmParams;
+    }
+
+    public static void destoryView() {
+        try {
+            if (mFloatLayout != null && mWindowManager != null)
+                mWindowManager.removeView(mFloatLayout);
+        }
+        catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
+    }
+}
