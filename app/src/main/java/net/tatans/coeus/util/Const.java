c19b0f781576150c42f03e36c997896ca2506aca
chenluyu <540302096@qq.com>
2016-08-12 15:25:03 +0800

    拨打电话通过phone_state广播监听

---
+++ app/src/main/java/net/tatans/coeus/util/Const.java
@@ -0,0 +1,10 @@
+package net.tatans.coeus.util;
+
+/**
+ * Created by cly on 2016/8/12.
+ */
+
+public class Const {
+    public static int PHONE_STATE = 0;
+    public static String EXTRA_PHONE_NUMBER = "";
+}
